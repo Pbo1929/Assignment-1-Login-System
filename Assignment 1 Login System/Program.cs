6a7f525d98f72ca9dd682dd6c7f18dfa56b35495
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-11 12:15:40 +0700

    Add project files.

---
+++ Assignment 1 Login System/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment_1_Login_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User bob = new User("Bobby", "Bob Ross", "bob123");
+            Console.WriteLine("User logged in:" + bob.loggedIn);
+
+            Console.Write(bob.CheckLogin("Adam", "adam123"));
+            Console.Write(bob.CheckLogin("Bobby", "adam123"));
+            Console.Write(bob.CheckLogin("Bobby", "bob123"));
+
+            Console.WriteLine("User logged in:" + bob.loggedIn);
+
+            Console.WriteLine(bob.fullusername);
+            bob.ChangePassword("brown123");
+            bob.LogOut();
+            Console.WriteLine(bob.CheckLogin("Bobby", "brown123"));
+        }
+    }
+}
---
+++ Assignment 1 Login System/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1_Login_System
+{
+    class User
+    {
+        private string username;
+        public string fullusername;
+        private string password;
+        public bool loggedIn = false;
+
+        public User(string un, string fu, string pw)// Constructor
+        {
+            username = un;
+            password = pw;
+            fullusername = fu;
+        }
+
+        public bool CheckLogin(string un, string pw)
+        {
+            if (un == username && pw == password)
+            {
+                loggedIn = true;
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        public void LogOut()
+        {
+            loggedIn = false;
+        }
+        public void ChangePassword(string newPassword)
+        {
+            if (loggedIn == true)
+            {
+                Console.WriteLine("SUCCESS");
+                password = newPassword;
+                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
